b6c644d6f7deae7baf976f78d0723b5e440733e7
W <k74972929@gmail.com>
2023-10-14 20:26:16 +0800

    收藏路段新增

--- app/src/main/java/ntutifm/game/google/entity/adaptor/NotificationAdaptor.kt
+++ app/src/main/java/ntutifm/game/google/entity/adaptor/NotificationAdaptor.kt
@@ -17,9 +17,9 @@ class NotificationAdaptor(private var mList: List<Incident>?, private val itemOn
 
     inner class IncidentHolder(binding: IncidentItemBinding): RecyclerView.ViewHolder(binding.root){
         val type : TextView = itemView.findViewById(R.id.title1)
-        val time : TextView = itemView.findViewById(R.id.raiseTime1)
-        val from : TextView = itemView.findViewById(R.id.source1)
-        val state : TextView = itemView.findViewById(R.id.state1)
+//        val time : TextView = itemView.findViewById(R.id.raiseTime1)
+//        val from : TextView = itemView.findViewById(R.id.source1)
+//        val state : TextView = itemView.findViewById(R.id.state1)
         val description : TextView = itemView.findViewById(R.id.content1)
         val root : MaterialCardView = itemView.findViewById(R.id.root)
     }
@@ -38,13 +38,13 @@ class NotificationAdaptor(private var mList: List<Incident>?, private val itemOn
     override fun onBindViewHolder(holder: NotificationAdaptor.IncidentHolder, position: Int) {
         holder.type.text = (mList?.get(position)?.part + mList?.get(position)?.type) ?: ""
         holder.description.text = mList?.get(position)?.title ?: ""
-        holder.time.text = mList?.get(position)?.raiseTime ?: ""
-        holder.from.text = ("來源:" + mList?.get(position)?.auth) ?: ""
-        holder.state.text = "(狀況:" +
-              (if(mList?.get(position)?.solved !="nxx"){
-                  mList?.get(position)?.solved
-              } else {" 未排除"} )  +
-         ")"
+//        holder.time.text = mList?.get(position)?.raiseTime ?: ""
+//        holder.from.text = ("來源:" + mList?.get(position)?.auth) ?: ""
+//        holder.state.text = "(狀況:" +
+//              (if(mList?.get(position)?.solved !="nxx"){
+//                  mList?.get(position)?.solved
+//              } else {" 未排除"} )  +
+//         ")"
         holder.root.tag = mList?.get(position) ?: "22"
         holder.root.setOnClickListener(itemOnClickListener)
 
